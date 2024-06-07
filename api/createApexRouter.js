88895021c0d34aafa5c996abd72f21f1fd2e1890
Ryan Gloff <ryan@ryangloff.com>
2024-06-07 17:22:49 -0400

    Added ilog

--- api/createApexRouter.js
+++ api/createApexRouter.js
@@ -5,13 +5,13 @@ import loadMockResponse from '../lib/loadMockResponse.js';
 import getConfig from '../lib/apex/getConfig.js';
 import getStatus from '../lib/apex/getStatus.js';
 import getTLog from '../lib/apex/getTLog.js';
+import getILog from '../lib/apex/getILog.js';
 
 const config = getConfiguration();
 
 function createApexRouter() {
   const router = express.Router();
 
-  console.log('Creating config endpoint');
   router.get('/config', async (req, res) => {
     if (config.apex.mockCalls) return res.json(loadMockResponse('/config.json'));
     res.json(await getConfig());
@@ -25,6 +25,12 @@ function createApexRouter() {
   router.get('/tlog', async (req, res) => {
     if (config.apex.mockCalls) return res.json(loadMockResponse('/tlog.json'));
     res.json(await getTLog(req.query.startDay, req.query.numDays));
+  });
+
+  router.get('/ilog', async (req, res) => {
+    console.log('ilog');
+    if (config.apex.mockCalls) return res.json(loadMockResponse('/ilog.json'));
+    res.json(await getILog(req.query.startDay, req.query.numDays));
   });
 
   return router;
