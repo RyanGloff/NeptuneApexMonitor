159deea4a965beeaed29424f924ab4fcd87250a2
Ryan Gloff <gloffrm@amazon.com>
2024-06-07 15:54:27 +0000

    Added system for mock commands. Still needs valid response data though

--- api/createApexRouter.js
+++ api/createApexRouter.js
@@ -1,20 +1,29 @@
 import express from 'express';
+import getConfiguration from '../lib/getConfiguration.js';
+import loadMockResponse from '../lib/loadMockResponse.js';
+
 import getConfig from '../lib/apex/getConfig.js';
 import getStatus from '../lib/apex/getStatus.js';
 import getTLog from '../lib/apex/getTLog.js';
 
+const config = getConfiguration();
+
 function createApexRouter() {
   const router = express.Router();
 
+  console.log('Creating config endpoint');
   router.get('/config', async (req, res) => {
-    res.json(await getApexConfig());
+    if (config.apex.mockCalls) return res.json(loadMockResponse('/config.json'));
+    res.json(await getConfig());
   });
 
   router.get('/status', async (req, res) => {
+    if (config.apex.mockCalls) return res.json(loadMockResponse('/status.json'));
     res.json(await getStatus());
   });
 
   router.get('/tlog', async (req, res) => {
+    if (config.apex.mockCalls) return res.json(loadMockResponse('/tlog.json'));
     res.json(await getTLog(req.query.startDay, req.query.numDays));
   });
 
